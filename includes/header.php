92925646d8dc012b6edbe3c02fbd6b18d4c89755
Kenneth <123523257+KenTree@users.noreply.github.com>
2025-12-11 22:19:12 +0000

    Added login header

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,14 @@
 <?php function theatre_header() { 
+     require_once __DIR__ . '/../includes/init.php';
+     $user = current_user();
+
+     $authLinks = '';
+     if ($user) {
+          $authLinks = '<a href="logout.php">Logout</a>';
+     } else {
+          $authLinks = '<a href="login.php">Login</a>
+               <a href="register.php">Register</a>';
+     }
 
 	return '
    <header id = "header-style">
@@ -7,6 +17,7 @@
             <a href="movies.php">Movies</a>
             <a href="my_tickets.php">Tickets</a>
             <a href="reports.php">Reports</a>
+            ' . $authLinks . '
        </nav>
   </header>
 	';
